07bea7b695039206604cc1c5731d94baae71fc93
Buğrahan Y. <78935299+bhy01dev@users.noreply.github.com>
2024-05-11 15:07:06 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,6 @@ export default function Page() {
       <p className="mb-4">
         {`I am Bugrahan, I was born in Istanbul in 2005. After completing my primary, secondary and high school education, I entered the Computer Programming department of Istanbul Topkapi University. I am currently continuing to develop myself in the field of Information Technologies such as Network Attacks and Web Security related to Kali Linux, a Linux distribution. In the future, I want to pursue a career as both a successful software developer and a talented cybersecurity researcher.`}
       </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
     </section>
   )
 }
