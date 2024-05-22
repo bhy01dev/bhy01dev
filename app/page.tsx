ef75273b7e712a54ad26d1d1cedf7132b4088e35
Buğrahan Y. <78935299+bhy01dev@users.noreply.github.com>
2024-05-22 19:20:24 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,9 @@ export default function Page() {
       <p className="mb-4">
         {`I am Bugrahan, I was born in Istanbul in 2005. After completing my primary, secondary and high school education, I entered the Computer Programming department of Istanbul Topkapi University. I am currently continuing to develop myself in the field of Information Technologies such as Network Attacks and Web Security related to Kali Linux, a Linux distribution. In the future, I want to pursue a career as both a successful software developer and a talented cybersecurity researcher.`}
       </p>
-      <script src="https://tryhackme.com/badge/3099866"></script>
+      <p>
+        <script src="https://tryhackme.com/badge/3099866"></script>
+      </p>
     </section>
   )
 }
